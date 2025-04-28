bc91a22bbcaf9f403368a4b051b9eefa7880628f
NEZUKO <trandinhvan0294@gmail.com>
2025-04-28 21:34:51 +0700

    .

--- check-domain/index.php
+++ check-domain/index.php
@@ -1,6 +1,16 @@
 <?php
-// Set response headers to JSON
+// Set response headers
 header('Content-Type: application/json');
+// Add CORS headers
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+// Handle preflight OPTIONS request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 // Check if the request method is GET
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {

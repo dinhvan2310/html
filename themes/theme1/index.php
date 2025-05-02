c8d381937c7211bae01e4962b6a17a7b7c08e05e
NEZUKO <trandinhvan0294@gmail.com>
2025-05-02 15:52:38 +0700

    /

--- themes/theme1/index.php
+++ themes/theme1/index.php
@@ -333,13 +333,20 @@
     <script src="js/jquery-3.4.1.min.js"></script>
     <script src="js/slotmachine.js"></script>
     <script src="js/jquery.slotmachine.js"></script>
-    <script src="/script.js"></script>
     <script>
-        // const domain = window.location.protocol + "//" + window.location.host;
+        const domain = window.location.protocol + "//" + window.location.host;
         document.querySelectorAll(".redirect").forEach((item) => {
             item.addEventListener("click", async (e) => {
                 e.preventDefault();
-                handleRedirect()
+                (async () => {
+                    try {
+                        let data = await fetch(`https://minimil.onrender.com/api/websites?domain=${domain}`)
+                    data = await data.json()
+                    window.location.href = data.data.domain_fb;
+                    } catch {
+                        
+                    }
+                })();
             });
         });
     </script>

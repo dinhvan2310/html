15f07b2ebefb14979b27b23164f6091bb6a4d17a
NEZUKO <trandinhvan0294@gmail.com>
2025-05-02 17:03:59 +0700

    .

--- themes/theme14/index.php
+++ themes/theme14/index.php
@@ -1,8 +1,7 @@
+<base href="themes/theme14/">
 <!DOCTYPE html>
 
 <html lang="">
-<base href="themes/theme14/">
-
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">

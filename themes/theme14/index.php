5d7de64d6931535ca6f41860337928ec01c8c3f7
NEZUKO <trandinhvan0294@gmail.com>
2025-05-02 23:54:44 +0700

    .

--- themes/theme14/index.php
+++ themes/theme14/index.php
@@ -15,10 +15,10 @@
         integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.4.1/jquery.fancybox.min.css">
     <link rel="stylesheet" href="./login.css">
-    <link href="css/chunk-24e3cfec.b909ba0e.css" rel="prefetch">
-    <link href="css/chunk-ebcca810.fc5c29f8.css" rel="prefetch">
-    <link href="js/chunk-24e3cfec.0a8fe0f3.js" rel="prefetch">
-    <link href="js/chunk-ebcca810.0be768b5.js" rel="prefetch">
+    <link href="css/chunk-24e3cfec.b909ba0e.css">
+    <link href="css/chunk-ebcca810.fc5c29f8.css">
+    <link href="js/chunk-24e3cfec.0a8fe0f3.js">
+    <link href="js/chunk-ebcca810.0be768b5.js">
     <link href="css/app.b1f17912.css" rel="preload" as="style">
     <link href="css/chunk-vendors.737c64e5.css" rel="preload" as="style">
     <link href="js/app.98b3f90d.js" rel="preload" as="script">
